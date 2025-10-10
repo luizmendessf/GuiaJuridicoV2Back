38f0a7ec6eddfaf62fe36f933234dba535025013
Luiz Mendes <luizmendessf@gmail.com>
2025-10-10 15:58:55 -0300

    build: remove invalid javac flag; align Lombok 1.18.34

--- src/main/java/org/guiajuridico/dto/ForgotPasswordRequestDto.java
+++
@@ -1,8 +0,0 @@
-package org.guiajuridico.dto;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class ForgotPasswordRequestDto {
-    private String email;
-}
--- src/main/java/org/guiajuridico/dto/ResetPasswordRequestDto.java
+++
@@ -1,9 +0,0 @@
-package org.guiajuridico.dto;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class ResetPasswordRequestDto {
-    private String token;
-    private String novaSenha;
-}
